83f8e778226ef381d49494a394f0b8eccab36895
Louis Daily <louis.daily@silvercar.com>
2019-09-24 15:38:38 -0500

    [RAC-10401] Add Delegation Pattern (#26)
    [RAC-10401] - Unleash Client Delegation
    * Added UnleashDelegate protocol and method stubs.
    
    * Reformatted file to follow Ray Wenderlich Swift Style Guide.
    
    * Updated initializer and method signatures to match Ray Wenderlich Swift Style guides.
    
    * Added delegate property and method invocations if delegate exists.
    
    * Updated sample app to use Unleash delegation pattern. Removed UnleashDelegate file.
    
    * Made requested updates based off of feedback from PR.

--- UnleashSampleApp/ViewController.swift
+++ UnleashSampleApp/ViewController.swift
@@ -10,34 +10,56 @@ import Unleash
 
 // This constant is to emulate a client knowing its target environment is (e.g. QA, Production)
 struct Constants {
-    static let environment: String = "QA"
+  static let environment: String = "QA"
 }
 
 class ViewController: UIViewController {
-    //MARK: Properties
-    @IBOutlet internal weak var isFeatureEnabled: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let unleash: Unleash = Unleash(appName: "ios-unleash-sample-app",
-                                       url: "https://unleash.silvercar.com/api",
-                                       refreshInterval: 300000,
-                                       strategies: [EnvironmentStrategy()])
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.isFeatureEnabled?.text = "\(unleash.isEnabled(name: "enable-auth0-in-admin-client"))"
-        }
+  //MARK: Properties
+  @IBOutlet internal var isFeatureEnabled: UILabel!
+  @IBOutlet internal var activityIndicator: UIActivityIndicatorView!
+  private var isLoading: Bool = false {
+    didSet {
+      isLoading
+      ? activityIndicator.startAnimating()
+      : activityIndicator.stopAnimating()
     }
+  }
+  private var unleash: Unleash!
+  
+  // MARK: - View Lifecycle
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    isLoading = true
+    unleash = Unleash(
+      appName: "ios-unleash-sample-app",
+      url: "https://unleash.silvercar.com/api",
+      refreshInterval: 300000,
+      strategies: [EnvironmentStrategy()])
+    unleash.delegate = self
+  }
+}
+
+// MARK: - UnleashDelegate
+extension ViewController: UnleashDelegate {
+  func unleashDidFetchToggles(_ unleash: Unleash) {
+    isLoading = false
+    isFeatureEnabled?.text = "\(unleash.isEnabled(name: "enable-auth0-in-admin-client"))"
+  }
+  
+  func unleash(_ unleash: Unleash, didFailWithError error: Error) {
+    isLoading = false
+    isFeatureEnabled?.text = "Unleash Failed: \(error.localizedDescription)"
+  }
 }
 
 class EnvironmentStrategy: Strategy {    
-    var name: String {
-        return "environment"
-    }
-    
-    func isEnabled(parameters: [String : String]) -> Bool {
-        return Constants.environment == parameters["environment"]
-    }
+  var name: String {
+    return "environment"
+  }
+  
+  func isEnabled(parameters: [String : String]) -> Bool {
+    return Constants.environment == parameters["environment"]
+  }
 }
 
--- unleash/Unleash.swift
+++ unleash/Unleash.swift
@@ -9,93 +9,125 @@ import Foundation
 import PMKFoundation
 import PromiseKit
 
-public protocol Strategy {
-    var name: String { get }
-    func isEnabled(parameters: [String: String]) -> Bool
+// MARK: - UnleashDelegate Protocol
+public protocol UnleashDelegate: AnyObject {
+  func unleashDidFetchToggles(_ unleash: Unleash)
+  func unleash(_ unleash: Unleash, didFailWithError error: Error)
 }
 
-public class Unleash {
-    private var registerService: RegisterServiceProtocol
-    private var toggleRepository: ToggleRepositoryProtocol
-    private var toggles: Toggles? { return toggleRepository.toggles }
-    
-    public private(set) var appName: String
-    public private(set) var url: String
-    public private(set) var refreshInterval: Int?
-    public private(set) var strategies: [Strategy]
-    
-    public convenience init(appName: String, url: String, refreshInterval: Int?, strategies: [Strategy] = []) {
-        let clientRegistration: ClientRegistration = ClientRegistration(appName: appName, strategies: strategies)
-        let registerService: RegisterServiceProtocol = RegisterService()
-        let memory: MemoryCache = MemoryCache(cache: Cache(), jsonDecoder: JSONDecoder(), jsonEncoder: JSONEncoder())
-        let toggleService: ToggleServiceProtocol
-            = ToggleService(appName: appName, instanceId: clientRegistration.instanceId)
-        let toggleRepository: ToggleRepository = ToggleRepository(memory: memory, toggleService: toggleService)
-        let allStrategies: [Strategy] = [DefaultStrategy()] + strategies
+// MARK: - Strategy
+public protocol Strategy {
+  var name: String { get }
+  func isEnabled(parameters: [String: String]) -> Bool
+}
 
-        self.init(clientRegistration: clientRegistration, registerService: registerService,
-                  toggleRepository: toggleRepository, appName: appName, url: url, refreshInterval: refreshInterval,
-                  strategies: allStrategies)
+// MARK: - Unleash
+public class Unleash {
+  
+  // MARK: - Properties
+  private var registerService: RegisterServiceProtocol
+  private var toggleRepository: ToggleRepositoryProtocol
+  private var toggles: Toggles? { return toggleRepository.toggles }
+  
+  public private(set) var appName: String
+  public private(set) var url: String
+  public private(set) var refreshInterval: Int?
+  public private(set) var strategies: [Strategy]
+  
+  public weak var delegate: UnleashDelegate?
+  
+  // MARK: - Lifecycle - Public Init
+  public convenience init(
+    appName: String,
+    url: String,
+    refreshInterval: Int?,
+    strategies: [Strategy] = []
+  ) {
+    let clientRegistration: ClientRegistration = ClientRegistration(appName: appName, strategies: strategies)
+    let registerService: RegisterServiceProtocol = RegisterService()
+    let memory: MemoryCache = MemoryCache(cache: Cache(), jsonDecoder: JSONDecoder(), jsonEncoder: JSONEncoder())
+    let toggleService: ToggleServiceProtocol = ToggleService(appName: appName, instanceId: clientRegistration.instanceId)
+    let toggleRepository: ToggleRepository = ToggleRepository(memory: memory, toggleService: toggleService)
+    let allStrategies: [Strategy] = [DefaultStrategy()] + strategies
+    
+    self.init(
+      clientRegistration: clientRegistration,
+      registerService: registerService,
+      toggleRepository: toggleRepository,
+      appName: appName,
+      url: url,
+      refreshInterval: refreshInterval,
+      strategies: allStrategies)
+  }
+  
+  // MARK: - Internal Init
+  init(
+    clientRegistration: ClientRegistration,
+    registerService: RegisterServiceProtocol,
+    toggleRepository: ToggleRepositoryProtocol,
+    appName: String,
+    url: String,
+    refreshInterval: Int?,
+    strategies: [Strategy]
+  ) {
+    self.registerService = registerService
+    self.toggleRepository = toggleRepository
+    self.appName = appName
+    self.url = url
+    self.refreshInterval = refreshInterval
+    self.strategies = strategies
+    
+    register(body: clientRegistration)
+    .then { self.toggleRepository.get(url: URL(string: url)!) }
+    .done { _ in self.delegate?.unleashDidFetchToggles(self) }
+    .catch { error in
+      log("error \(error)")
+      self.delegate?.unleash(self, didFailWithError: error)
+    }
+  }
+  
+  // MARK: - Register
+  private func register(body: ClientRegistration) -> Promise<Void> {
+    return attempt(maximumRetryCount: 3, delayBeforeRetry: .seconds(60)) {
+      self.registerService.register(url: URL(string: self.url)!, body: body)
+    }.done { response in
+      log("Unleash registered client \(body.instanceId) with response \(response ?? [:])")
+    }
+  }
+  
+  // MARK: - Attempt
+  private func attempt<T>(
+    maximumRetryCount: Int,
+    delayBeforeRetry: DispatchTimeInterval,
+    _ body: @escaping () -> Promise<T>
+  ) -> Promise<T> {
+    var attempts = 0
+    func attempt() -> Promise<T> {
+      attempts += 1
+      return body().recover { error -> Promise<T> in
+        guard attempts < maximumRetryCount else { throw error }
+        return after(delayBeforeRetry).then(on: nil, attempt)
+      }
+    }
+    return attempt()
+  }
+  
+  // MARK: - Is Enabled
+  public func isEnabled(name: String) -> Bool {
+    guard
+      let feature = toggles?.features.first(where: { $0.name == name }),
+      feature.enabled
+      else { return false }
+    
+    for strategy in feature.strategies {
+      guard let targetStrategy = strategies.first(where: { $0.name == strategy.name }) else { continue }
+      guard let parameters = strategy.parameters else { continue }
+      
+      if targetStrategy.isEnabled(parameters: parameters) {
+        return true
+      }
     }
     
-    init(clientRegistration: ClientRegistration, registerService: RegisterServiceProtocol,
-         toggleRepository: ToggleRepositoryProtocol, appName: String, url: String, refreshInterval: Int?,
-         strategies: [Strategy]) {
-        
-        self.registerService = registerService
-        self.toggleRepository = toggleRepository
-        self.appName = appName
-        self.url = url
-        self.refreshInterval = refreshInterval
-        self.strategies = strategies
-        
-        register(body: clientRegistration)
-            .then { _ in
-                self.toggleRepository.get(url: URL(string: url)!)
-            }
-            .catch { error in
-                log("error \(error)")
-        }
-    }
-    
-    private func register(body: ClientRegistration) -> Promise<Void> {
-        return attempt(maximumRetryCount: 3, delayBeforeRetry: .seconds(60)) {
-            self.registerService.register(url: URL(string: self.url)!, body: body)
-        }.done { response in
-            log("Unleash registered client \(body.instanceId) with response \(response ?? [:])")
-        }
-    }
-    
-    private func attempt<T>(maximumRetryCount: Int,
-                            delayBeforeRetry: DispatchTimeInterval,
-                            _ body: @escaping () -> Promise<T>) -> Promise<T> {
-        var attempts = 0
-        func attempt() -> Promise<T> {
-            attempts += 1
-            return body().recover { error -> Promise<T> in
-                guard attempts < maximumRetryCount else { throw error }
-                return after(delayBeforeRetry).then(on: nil, attempt)
-            }
-        }
-        return attempt()
-    }
-    
-    public func isEnabled(name: String) -> Bool {
-        guard let feature = toggles?.features.first(where: { $0.name == name }) else { return false }
-        
-        if !feature.enabled {
-            return false
-        }
-        
-        for strategy in feature.strategies {
-            guard let targetStrategy = strategies.first(where: { $0.name == strategy.name }) else { continue }
-            guard let parameters = strategy.parameters else { continue }
-            
-            if targetStrategy.isEnabled(parameters: parameters) {
-                return true
-            }
-        }
-        
-        return false
-    }
+    return false
+  }
 }
