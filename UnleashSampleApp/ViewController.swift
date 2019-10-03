37c62ecd113ceedd1b9cb8e79deb31aeca53eeb6
Louis Daily <louis.daily@silvercar.com>
2019-10-03 18:55:53 -0500

    Removed refresh button from main storyboard. Added delegation logic.

--- UnleashSampleApp/ViewController.swift
+++ UnleashSampleApp/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import Unleash
 
-// This constant is to emulate a client knowing its target environment is (e.g. QA, Production)
 struct Constants {
+  // This constant is to emulate a client knowing its target environment is (e.g. qa, stage, production)
   static let environment: String = "qa"
   static let appName = "ios-unleash-sample-app"
   static let unleashUrl = "https://unleash.silvercar.com/api"
   static let featureToggleName = "enable-ios-unleash-sample-app"
+  static let errorTitle = "Unleash Error"
 }
 
 class ViewController: UIViewController {
@@ -39,11 +40,10 @@ class ViewController: UIViewController {
       url: Constants.unleashUrl,
       refreshInterval: 10,
       strategies: [EnvironmentStrategy()])
-    isLoading = false
-    isFeatureEnabled.text = "\(unleash.isEnabled(name: ""))"
+    unleash.delegate = self
   }
   
-  @IBAction func didTapRefresh(_ sender: UIButton) {
+  private func refresh() {
     isFeatureEnabled.text = ""
     isLoading = true
     isFeatureEnabled.text = "\(unleash.isEnabled(name: Constants.featureToggleName))"
@@ -51,7 +51,25 @@ class ViewController: UIViewController {
   }
 }
 
-// MARK: - EnvironmentStrategy
+// MARK: UnleashDelegate
+extension ViewController: UnleashDelegate {
+  func unleashDidLoad(_ unleash: Unleash) {
+    refresh()
+  }
+  
+  func unleashDidFail(_ unleash: Unleash, withError error: Error) {
+    let alertController = UIAlertController(
+      title: Constants.errorTitle,
+      message: error.localizedDescription,
+      preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alertController.addAction(action)
+    present(alertController, animated: true, completion: nil)
+  }
+}
+
+// MARK: EnvironmentStrategy
 class EnvironmentStrategy: Strategy {    
   var name: String {
     return "environment"
